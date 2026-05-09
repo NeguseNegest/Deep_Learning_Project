99e3dac74fb0245a3de4e60874c8b568be1777d9
Jonathan Larsson <jonathla@kth.se>
2026-05-09 22:24:29 +0200

    test

---
+++ vp_sde.py
@@ -0,0 +1,107 @@
+import torch
+import math
+
+class VP_SDE:
+    def __init__(self, params):
+        self.beta_min = params['beta_min'] #b0 = 0.1 in experiment
+        self.beta_max = params['beta_max'] #bN = 20 in experiment
+        self.N = params['time_steps'] #N = 1000 in experiment
+        self.dt = 1 / self.N
+        self.sqrt_dt = torch.sqrt(self.dt)
+    
+    def sde(self, x, t):
+        r""""Returns the drift and diffusion of VP SDE"""
+        beta_t = self.beta_min + t * ( self.beta_max - self.beta_min)
+        drift = - 0.5 * (beta_t[:, None, None, None]) * x 
+        diffusion = torch.sqrt(beta_t)
+
+        return drift, diffusion
+    
+
+    def perturbation_kernel(self, x0, t):
+        r"""Returns the pertubation kernel p_{0t}(x(t) | x(0))"""
+        int_beta = (self.beta_min * t + 0.5 * t**2 * (self.beta_max - self.beta_min))
+    
+        mean = torch.exp(-0.5 * int_beta) * x0
+        var = 1 - torch.exp(-int_beta)
+    
+        return mean, var
+    
+    def sde_disc(self, x ,t):
+        r"""In equation dX = f(t, X)dt + g(t, X)dW, then this produces \
+        f(t,X)dt, g(t, X) \sqrt{dt}. Note that dW := N(0,I) * \sqrt{dt}!"""
+        drift, diffusion = self.sde(x, t)
+        return drift * self.dt + diffusion * self.sqrt_dt
+    
+    def reverse_sde(self, x, t, score):
+        r"""Returns the drift and diffusion for the reverse SDE"""
+        forward_drift, forward_diffusion = self.sde(x, t)
+        score_val = score(x, t)
+        reverse_drift = forward_drift - forward_diffusion[:, None, None, None]**2 * score_val
+        reverse_diffusion = forward_diffusion
+        return reverse_drift, reverse_diffusion
+    
+    def reverse_sde_disc(self, x, t):
+        pass
+
+    def normal_dist_sample(self, shape):
+        return torch.randn_like(shape)
+
+    
+
+
+def score_function(model, sde):
+    def new_score(x, t):
+        score = model(x, t)
+        x0 = torch.zeros_like(x)
+        _, std = sde.perturbation_kernel(x0, t)
+        return score / (std[:, None, None, None] )
+    return new_score
+
+
+
+#Their loss function
+def get_loss_fn(sde, eps=1e-5):
+    def loss_fn(model, batch):
+        score_fn = score_function(sde, model)
+
+        t = torch.rand(batch.shape[0], device=batch.device) * (sde.T - eps) + eps
+        z = torch.randn_like(batch)
+        mean, std = sde.marginal_prob(batch, t)
+        perturbed_data = mean + std[:, None, None, None] * z
+        score = score_fn(perturbed_data, t)
+
+        losses = torch.square(score * std[:, None, None, None] + z)
+        losses = torch.mean(losses.reshape(losses.shape[0], -1), dim=-1)
+        return torch.mean(losses)
+    
+    return loss_fn
+
+
+class EulerForward:
+    def __init__(self):
+        pass
+
+    def new_sample(self, x, t, score_function, model_sde):
+        r"""Euler forward for VP SDE"""
+        backward_dt = - model_sde.dt
+        drift, diff = model_sde.reverse_sde(x, t, score_function)
+        z = torch.randn_like(x)
+        x_new = x + drift * backward_dt + diff[:, None, None, None] * torch.sqrt(- backward_dt) * z
+        return x_new
+    
+
+@torch.no_grad()
+def get_samples(x, score, sde, examples, eul_for, eps = 0.01):
+    batch_size = x.shape[0]
+    t = torch.ones((batch_size,), device = x.device)
+    x_old = sde.normal_dist_sample(x)
+    N = sde.N
+    t_i = torch.linspace(1, eps, N)
+    lists_of_progress = []
+    for i in range(N):
+        x_new = eul_for.new_sample(x_old, t, score, sde) * t_i[i]
+        x_old = x_new
+        if ( i % ( N // examples) == 0):
+            lists_of_progress.append(x_old)
+    return x_new, lists_of_progress
